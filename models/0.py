448f81059fb61ad0371ac6af98e87a7e3ece1d35
Jhon <ladillagain@gmail.com>
2018-04-05 22:06:08 -0400

    Subida Base

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'ex_controller'
+settings.subtitle = 'powered by web2py'
+settings.author = 'you'
+settings.author_email = 'you@example.com'
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '0bd94799-82da-4082-8a08-7662523b0d87'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
